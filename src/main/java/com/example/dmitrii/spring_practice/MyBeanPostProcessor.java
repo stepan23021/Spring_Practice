8b216228855c70542bebb0f37555086584045852
Dmitrii <stepan23021@gmail.com>
2018-09-03 15:44:45 +0300

    Added BPP

---
+++ src/main/java/com/example/dmitrii/spring_practice/MyBeanPostProcessor.java
@@ -0,0 +1,21 @@
+package com.example.dmitrii.spring_practice;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanPostProcessor;
+
+/**
+ * @author Dmitrii Stepcenco
+ * @created 03 September 2018
+ **/
+
+public class MyBeanPostProcessor implements BeanPostProcessor {
+    @Override
+    public Object postProcessBeforeInitialization(Object bean, String beanName) throws BeansException {
+        return bean;
+    }
+
+    @Override
+    public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
+        return bean;
+    }
+}
