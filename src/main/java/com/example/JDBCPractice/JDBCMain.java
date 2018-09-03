8308aa2e45a7198628f797b77001773b390c86ad
stepan23021 <stepan23021@gmail.com>
2018-09-03 10:53:45 +0300

    Test

--- src/main/java/com/example/JDBCPractice/JDBCMain.java
+++ src/main/java/com/example/JDBCPractice/JDBCMain.java
@@ -90,6 +90,7 @@ public class JDBCMain {
                             checker = true;
                             break;
                     }
+                    System.out.println("");
                     statement.close();
                     connection.close();
 
