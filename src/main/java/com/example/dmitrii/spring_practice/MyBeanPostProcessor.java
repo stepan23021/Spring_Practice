481b96d8ce7a8364f0c10071c069f09cb6daf115
Dmitrii <stepan23021@gmail.com>
2018-09-03 17:12:30 +0300

    First commit on Spring Branch

--- src/main/java/com/example/dmitrii/spring_practice/MyBeanPostProcessor.java
+++ src/main/java/com/example/dmitrii/spring_practice/MyBeanPostProcessor.java
@@ -11,6 +11,7 @@ import org.springframework.beans.factory.config.BeanPostProcessor;
 public class MyBeanPostProcessor implements BeanPostProcessor {
     @Override
     public Object postProcessBeforeInitialization(Object bean, String beanName) throws BeansException {
+        System.out.println(bean + "Before initialization");
         return bean;
     }
 
