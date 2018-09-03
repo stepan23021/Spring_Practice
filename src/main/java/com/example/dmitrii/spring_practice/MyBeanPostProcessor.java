9134ef63dab33c83c5293f54336e8dfe2e93dfc5
Dmitrii <stepan23021@gmail.com>
2018-09-03 17:10:36 +0300

    Fix BPP

--- src/main/java/com/example/dmitrii/spring_practice/MyBeanPostProcessor.java
+++ src/main/java/com/example/dmitrii/spring_practice/MyBeanPostProcessor.java
@@ -16,6 +16,7 @@ public class MyBeanPostProcessor implements BeanPostProcessor {
 
     @Override
     public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
+        System.out.println(bean + "Post Processing");
         return bean;
     }
 }
