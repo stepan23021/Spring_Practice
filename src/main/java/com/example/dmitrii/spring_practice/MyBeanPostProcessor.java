5c638d0f9da4d86df862e0eb1e624f293b356a63
Dmitrii <stepan23021@gmail.com>
2018-09-05 10:10:29 +0300

    New

--- src/main/java/com/example/dmitrii/spring_practice/MyBeanPostProcessor.java
+++ src/main/java/com/example/dmitrii/spring_practice/MyBeanPostProcessor.java
@@ -15,27 +15,14 @@ public class MyBeanPostProcessor implements BeanPostProcessor {
     @Override
     public Object postProcessBeforeInitialization(Object bean, String beanName) throws BeansException {
         System.out.println(bean + " --postProcessBeforeInitialization()");
-        Class<?> beanClass = beanName.getClass();
-        Method[] beanClassMethods = beanClass.getMethods();
-        for (Method beanClassMethod : beanClassMethods) {
-            if(beanClassMethod.isAnnotationPresent(MyCustomAnnotation.class)){
-                System.out.println("АННОТАЦИЯ НАШЛАСЬ В BeanPostProcessor");
-            }
-        }
+
         return bean;
     }
 
     @Override
     public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
         System.out.println(bean + "postProcessAfterInitialization()");
-
-        Class<?> beanClass = beanName.getClass();
-        Method[] beanClassMethods = beanClass.getMethods();
-        for (Method beanClassMethod : beanClassMethods) {
-            if(beanClassMethod.isAnnotationPresent(MyCustomAnnotation.class)){
-                System.out.println("АННОТАЦИЯ НАШЛАСЬ В BeanPostProcessor");
-            }
-        }
+        
         return bean;
     }
 }
